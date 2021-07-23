335f33ab4e07a7e0de6e3817c2ea27401ef217d8
TraceRicholson <trace.richolson@spaceforce.mil>
2021-07-23 11:29:57 -0500

    update API URLs to reflect Heroku

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -3,15 +3,19 @@
 
 const prod = {
   url: {
-    API_URL: 'https://hey-best-friend-bend.herokuapp.com',
-    API_URL_MESSAGES: ''}
-  };
+    API_URL: 'https://resfeber-studios-server.herokuapp.com/photos',
+    API_URL_LIVE: 'https://resfeber-studios-server.herokuapp.com/photos/live',
+    API_URL_NATURE: 'https://resfeber-studios-server.herokuapp.com/photos/nature',
+    API_URL_URBAN: 'https://resfeber-studios-server.herokuapp.com/photos/urban',
+  }
+};
 
-  const dev = {
+const dev = {
     url: {
     API_URL: 'http://localhost:5000/photos',
     API_URL_LIVE: 'http://localhost:5000/photos/live',
-    API_URL_NATURE: 'http://localhost:5000/photos/nature' 
+    API_URL_NATURE: 'http://localhost:5000/photos/nature',
+    API_URL_URBAN: 'http://localhost:5000/photos/urban',
     }
   };
 
