42c9924580f834a1fc6e578a783c4496482c5842
TraceRicholson <trace.richolson@spaceforce.mil>
2021-07-24 11:36:06 -0500

    update footer, clean imports on footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,23 +1,26 @@
-import { AppBar } from "@material-ui/core"
-import { Container } from "@material-ui/core"
-import { Toolbar } from "@material-ui/core"
-import { Typography } from "@material-ui/core"
-import { makeStyles } from "@material-ui/core"
+import React from 'react'
 import InstagramIcon from '@material-ui/icons/Instagram';
 import YouTubeIcon from '@material-ui/icons/YouTube';
 import { Link } from "react-router-dom"
-import { Button } from "@material-ui/core"
+import { Button, BottomNavigation, makeStyles} from "@material-ui/core"
+
 
 const useStyles = makeStyles((theme) => ({
   bar: {
     backgroundColor: '#a78824',
     elevation: '3',
-    alignItems: 'center'
+    alignItems: 'center',
+    position: "relative",
+    top: 850,
+    left: 0,
+    bottom: 0
+
   },
   link: {
     textDecoration: 'none',
     color: 'white',
     fontWeight: "bold",
+    fontSize: "1vw"
 
   },
   copy: {
@@ -25,26 +28,44 @@ const useStyles = makeStyles((theme) => ({
     fontSize: "1.3vw",
     color: "white",
 
-  }
+  },
+  stickToBottom: {
+    fontFamily: 'Prestage',
+    fontSize: "1.3vw",
+    color: "white",
+    width: '100%',
+    position: 'fixed',
+    bottom: 0,
+    backgroundColor: '#a78824',
+    elevation: '3',
+  },
 }))
 export default function Footer() {
   const classes = useStyles();
+  const [value, setValue] = React.useState(0);
   return (
-    <AppBar className={classes.bar} position="static">
-      <Toolbar className={classes.copy}>
-          <Link to="/admin" className={classes.link}>
-            <Button color="inherit">Admin</Button>
-          </Link>
-        © 2021 Resfeber Stuidos
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      showLabels
+      className={classes.stickToBottom}
+    >
+      <Link to="/admin" className={classes.link}>
+        <Button color="inherit" className={classes.copy}>Admin</Button>
+      </Link>
 
-          <Button color="inherit" href='https://www.youtube.com/channel/UClc0S_x5xcPCrmNrlFGXnCQ'>
-            <YouTubeIcon/>
-          </Button>
-          <Button color="inherit" href='https://www.instagram.com/resfeber_studios/'>
-            <InstagramIcon/>
-          </Button>
+      <p className={classes.link}>© 2021 Resfeber Stuidos</p>
 
-      </Toolbar>
-    </AppBar>
+      <Button color="inherit" href='https://www.youtube.com/channel/UClc0S_x5xcPCrmNrlFGXnCQ'>
+        <YouTubeIcon/>
+      </Button>
+      <Button color="inherit" href='https://www.instagram.com/resfeber_studios/'>
+        <InstagramIcon/>
+      </Button>
+    </BottomNavigation>
   )
 }
+
+
